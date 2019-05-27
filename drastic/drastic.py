70ab58bc675d67fc9af09fb9bc00d369023f5983
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2019-05-27 11:07:53 +0000

    Run specific recipes

--- drastic/drastic.py
+++ drastic/drastic.py
@@ -29,11 +29,26 @@ def json_dump(data_dict, filename='recipes.json', root='output'):
     repeated recipes will be replaced.
     """
     filename = ('{:s}/{:s}'.format(root, filename))
+    num = int(data_dict.keys()[0].split('_')[0])
     try:
         # Extract data from the json data file
         with open(filename) as data_file:
             data_existing = json.load(data_file)
-            data_existing.update(data_dict)
+            counter = 0
+            for d in sorted(data_existing):
+                counter+=1
+                cab_num = int(d.split('_')[0])
+                if num == cab_num:
+                    old_recipe = data_existing.pop(d)
+                    data_existing.update(data_dict)
+                    recipe_name = d.split('_')[-1]
+                    data_existing['{:d}_{:s}'.format(counter+1, recipe_name)] = old_recipe
+                elif num < cab_num:
+                    old_recipe = data_existing.pop(d)
+                    recipe_name = d.split('_')[-1]
+                    data_existing['{:d}_{:s}'.format(counter+1, recipe_name)] = old_recipe
+                else:
+                    data_existing.update(data_dict)
             data = data_existing
     except IOError:
         data = data_dict
@@ -53,18 +68,23 @@ def get_data(filename='', root='output'):
         data = json.load(f)
     return data
 
-def get_cab_num(recipes_file='', num=0):
+
+def get_cab_num(num, recipes_file=''):
     if not num:
-        data = get_data(filename)
-        # get number based on current recipes
-    else:
-        num
+        try:
+            data = get_data(recipes_file)
+            for d in sorted(data):
+                cab_num = int(d.split('_')[0])
+                num = cab_num+1
+        except IOError:
+            num = 1
+    return num
 
 # 1: Create a telescope measurement set
 
 def simms(recipe, num, parameters, recipes_file=''):
-    num = get_cab_num(num, recipes_file)
     recipe_params = {}
+    num = get_cab_num(num, recipes_file)
     step = "{}_simulated_ms".format(num)
     cab = '{}_simms'.format(num)
     recipe.add("cab/simms",
@@ -84,8 +104,9 @@ def simms(recipe, num, parameters, recipes_file=''):
 
 def simulator(recipe, num, parameters, recipes_file=''):
     recipe_params = {}
+    num = get_cab_num(num, recipes_file)
     step = "{}_generate_data".format(num)
-    cab = '{}_simms'.format(num)
+    cab = '{}_simulator'.format(num)
     recipe.add("cab/simulator",
                step,
                parameters,
@@ -103,6 +124,7 @@ def simulator(recipe, num, parameters, recipes_file=''):
 
 def meqtrees(recipe, num, parameters, recipes_file=''):
     recipe_params = {}
+    num = get_cab_num(num, recipes_file)
     step = "{}_meq_cal".format(num)
     cab = '{}_calibrator'.format(num)
     recipe.add('cab/calibrator',
@@ -120,6 +142,7 @@ def meqtrees(recipe, num, parameters, recipes_file=''):
 
 def cubical(recipe, num, parameters):
     recipe_params = {}
+    num = get_cab_num(num, recipes_file)
     step = "{}_cub_cal".format(num)
     cab = '{}_cubical'.format(num)
     recipe.add('cab/cubical',
@@ -140,6 +163,7 @@ def cubical(recipe, num, parameters):
 
 def wsclean(recipe, num, parameters, recipes_file=''):
     recipe_params = {}
+    num = get_cab_num(num, recipes_file)
     step = "{}_image_wsclean".format(num)
     cab = '{}_wsclean'.format(num)
     recipe.add('cab/wsclean',
@@ -157,6 +181,7 @@ def wsclean(recipe, num, parameters, recipes_file=''):
 
 def casa_tclean(recipe, num, parameters, recipes_file=''):
     recipe_params = {}
+    num = get_cab_num(num, recipes_file)
     step = "{}_image_tclean".format(num)
     cab = '{}_casa_tclean'.format(num)
     recipe.add('cab/casa_tclean',
@@ -174,6 +199,7 @@ def casa_tclean(recipe, num, parameters, recipes_file=''):
 
 def casa_clean(recipe, num, parameters, recipes_file=''):
     recipe_params = {}
+    num = get_cab_num(num, recipes_file)
     step = "{}_image_clean".format(num)
     cab = '{}_casa_clean'.format(num)
     recipe.add('cab/casa_clean',
@@ -191,6 +217,7 @@ def casa_clean(recipe, num, parameters, recipes_file=''):
 
 def ddfacet(recipe, num, parameters, recipes_file=''):
     recipe_params = {}
+    num = get_cab_num(num, recipes_file)
     step = "{}_image_ddfacet".format(num)
     cab = '{}_ddfacet'.format(num)
     recipe.add('cab/ddfacet',
@@ -208,6 +235,7 @@ def ddfacet(recipe, num, parameters, recipes_file=''):
 
 def lwimager(recipe, num, parameters, recipes_file=''):
     recipe_params = {}
+    num = get_cab_num(num, recipes_file)
     step = "{}_image_lwimager".format(num)
     cab = '{}_lwimager'.format(num)
     recipe.add('cab/lwimager',
@@ -227,6 +255,7 @@ def lwimager(recipe, num, parameters, recipes_file=''):
 
 def casa_makemask(recipe, num, parameters, recipes_file=''):
     recipe_params = {}
+    num = get_cab_num(num, recipes_file)
     step = "{}_MakeMask".format(num)
     cab = '{}_casa_makemask'.format(num)
     recipe.add('cab/casa_makemask',
@@ -244,6 +273,7 @@ def casa_makemask(recipe, num, parameters, recipes_file=''):
 
 def cleanmask(recipe, num, parameters, recipes_file=''):
     recipe_params = {}
+    num = get_cab_num(num, recipes_file)
     step = "{}_CleanMask".format(num)
     cab = '{}_cleanmask'.format(num)
     recipe.add('cab/cleanmask',
@@ -263,6 +293,7 @@ def cleanmask(recipe, num, parameters, recipes_file=''):
 
 def fitstools(recipe, num, parameters, recipes_file=''):
     recipe_params = {}
+    num = get_cab_num(num, recipes_file)
     step = "{}_make_cubes".format(num)
     cab = '{}_fitstool'.format(num)
     recipe.add('cab/fitstool',
@@ -282,6 +313,7 @@ def fitstools(recipe, num, parameters, recipes_file=''):
 
 def pybdsm(recipe, num, parameters, recipes_file=''):
     recipe_params = {}
+    num = get_cab_num(num, recipes_file)
     step = "{}_pybdsm_finder".format(num)
     cab = '{}_pybdsm'.format(num)
     recipe.add('cab/pybdsm',
@@ -299,6 +331,7 @@ def pybdsm(recipe, num, parameters, recipes_file=''):
 
 def aegean(recipe, num, parameters, recipes_file=''):
     recipe_params = {}
+    num = get_cab_num(num, recipes_file)
     step = "{}_aegean_finder".format(num)
     cab = '{}_aegean'.format(num)
     recipe.add('cab/aegean',
