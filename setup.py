45b77ac4d596c8ca9d307e3b16149df8305a2b60
Athanaseus Javas Ramaila <aramaila@ska.ac.za>
2019-08-26 09:22:12 +0000

    Add requirements file as part of package resources

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 pkg = 'drasticali'
-__version__ = '0.0.8'
+__version__ = '0.0.9'
 
 build_root = os.path.dirname(__file__)
 scripts = ["bin/" + j for j in os.listdir("bin") ]
